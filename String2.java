67989fa1bc67b2c47cc58bb869adb3765a09dd09
Prashant Singh <134587691+prashantsingh05@users.noreply.github.com>
2023-08-16 20:59:06 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,8 @@
+public class String2 {
+    public static void main(String[] args) {
+        // String
+        // chatAt (Position finding)
+        String name = "Prashant";
+        System.out.println(name.charAt(5));
+    }
+}
